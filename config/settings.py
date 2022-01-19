bbd8a7763b46f45ea4103c917f29e2f76aef2213
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-01-19 10:53:23 +0900

    アプリケーション作成～ブラウザの表示 python manage.py startapp mysite

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-zo2+h1hti75pbjpwbgrz(bigvo^&!_c81i49j&k-4dl$0zjsll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite'
 ]
 
 MIDDLEWARE = [
