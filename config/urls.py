842c1cbad087ae484b14339f67dc2429ae36567c
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-01-19 17:28:46 +0900

    スーパーユーザーの作成 python manage.py createsuperuser

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from mysite import views
 
 # example.com/
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index),
     path('blog/', include('blog.urls'))
 ]
