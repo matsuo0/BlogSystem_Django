8a3822f8f4a180efb74b08d548eecba8a745d63a
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-01-19 11:09:18 +0900

    テンプレートとRenderの追加 ・TEMPLATESのDIRSへPathの追加

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>hello Really Site</h1>')
+    context = {
+        'title': 'Really Site'
+    }
+    return render(request, 'mysite/index.html', context)
