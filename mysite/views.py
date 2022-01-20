9e041fb8c665de590dd539c22c539f6f98d9589a
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-01-20 17:19:49 +0900

    記事を表示

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Article
 
 
 def index(request):
+    objs = Article.objects.all()  # 全ての記事を取得
     context = {
-        'title': 'Really Site'
+        'title': 'Really Site',
+        'articles': objs,
     }
     return render(request, 'mysite/index.html', context)
