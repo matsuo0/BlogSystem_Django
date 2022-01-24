1ef69d90ddbe5531e5ea9edab1f925e63b6ff1b3
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-01-24 14:53:00 +0900

    paginationの作成

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Article
+from django.core.paginator import Paginator
 
 
 def index(request):
     objs = Article.objects.all()
+    paginator = Paginator(objs, 2)  # 1ページに表示する記事数 2
+    page_number = request.GET.get('page')
     context = {
-        'articles': objs,
+        'page_obj': paginator.get_page(page_number),
+        'page_number': page_number,
     }
     return render(request, 'blog/blogs.html', context)
 
