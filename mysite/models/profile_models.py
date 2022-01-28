69267adf113e40def4c7624545a6da4fe679ae06
matsuo0 <uno831@gmail.com>
2022-01-28 22:29:10 +0900

    ProfileとUserモデルをOneToOneで関連付け

--- mysite/models/profile_models.py
+++ mysite/models/profile_models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), unique=True, on_delete=models.CASCADE, primary_key=True)
+
     username = models.CharField(default="匿名ユーザー", max_length=30)
 
     zipcode = models.CharField(default="", max_length=8)
