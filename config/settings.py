7f83ab8dd7bce54d20f092f52b64d2876edd72f4
Tetsuji Matsuo <tetsuji.matsuo@cheetahdigital.com>
2022-02-01 12:20:33 +0900

    staticの設定/画像の追加

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# --- static 設定項目 ---
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# --- static 設定項目 ---
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
